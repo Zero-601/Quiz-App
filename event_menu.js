4bd929f013b63f62dd6c0212d759be1eb8b651cc
Zero <andrianow817@gmail.com>
2024-06-10 22:41:42 +0700

    mulai puyeng

--- event_menu.js
+++ event_menu.js
@@ -1,14 +1,32 @@
+import { response } from "./quiz.js";
 const start_btn = document.getElementsByClassName("btn-start")[0];
 const popup_box = document.getElementsByClassName("box-difficulty ")[0];
 const popup_area = document.getElementsByClassName("popup-area")[0];
+
+const box_difficulty = document.getElementsByClassName("box-difficulty")[0];
+const box_quiz = document.getElementsByClassName("box-quiz")[0];
+
 const back_btn = document.getElementsByClassName("back-btn")[0];
+const close_btn = document.getElementsByClassName("close-btn")[0];
+const easy_btn = document.getElementsByClassName("easy-btn")[0];
 
 start_btn.addEventListener("click", (event) => {
   popup_area.style.display = "flex";
-  popup_box.style.display = "flex";
+  box_difficulty.style.display = "flex";
 
   back_btn.addEventListener("click", () => {
     popup_area.style.display = "none";
-    popup_box.style.display = "none";
+    box_difficulty.style.display = "none";
+  });
+
+  easy_btn.addEventListener("click", () => {
+    box_difficulty.style.display = "none";
+    box_quiz.style.display = "flex";
+    response();
+
+    close_btn.addEventListener("click", () => {
+      popup_area.style.display = "none";
+      box_quiz.style.display = "none";
+    });
   });
 });
--- quiz.js
+++ quiz.js
@@ -1,27 +1,31 @@
 const quiz_content = document.getElementsByClassName("quiz-content")[0];
 
 const quiz_component = (data, i) => {
+  let multiple = [...data.incorrect_answers, data.correct_answer];
   return `<div class="quiz-area">
     <h4> ${i + 1}. ${data.question}</h4>
-    
-  </div>`;
+    <div>${multiple.map((data) => `<button>${data}</button>`)}</div>
+    </div>`;
 };
 
-fetch(
-  "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple"
-)
-  .then((res) => res.json())
-  .then((data) => {
-    let component = data.results.map((value, i) => {
-      console.log(value);
-      return quiz_component(value, i);
+export const response = async () => {
+  return await fetch(
+    "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple"
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      let component = data.results.map((value, i) => {
+        return quiz_component(value, i);
+      });
+      quiz_content.innerHTML = component.join(" ");
+    })
+    .catch((e) => {
+      setTimeout(
+        () => (alert(e.message) == undefined ? location.reload() : null),
+        3000
+      );
     });
-    quiz_content.innerHTML = component.join(" ");
-  })
-  .catch((e) => {
-    setTimeout(
-      () =>
-        alert("Terjadi Kesalahan") == undefined ? location.reload() : null,
-      3000
-    );
-  });
+};
